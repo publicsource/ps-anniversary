268eaa307ae4f90c785870cd09b8c7aba8f9f270
NATASHA VICENS <natasha@NATASHAs-MBP.localdomain>
2019-10-30 15:32:30 -0400

    added fullpage, might not be working

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,54 @@
 // require("./lib/pym");
 
+////////// fullPage.js
+// Optional. When using fullPage extensions
+// require("./lib/fullpage.dragAndMove.min.js");
+require("./lib/scrolloverflow.js");
+
+// Optional. When using scrollOverflow:true
+// require('fullpage.js/vendors/scrolloverflow');
+
+//var fullpage = require('fullpage.js');
+
+// When using fullPage extensions replace the previos require
+// of fullpage.js for this file
+var fullpage = require("./lib/fullpage.extensions.min");
+
+// Initializing it
+var myFullpage = new fullpage('#fullpage', {
+    licenseKey: 'D8303C79-0BD64932-AEC886F3-4E1AAB2D',
+    anchors: ['page1', 'page2', 'page3'],
+    sectionsColor: ['#000000', '#000000', '#000000'],
+    slidesNavigation:true,
+    navigation: true,
+    navigationPosition: 'right',
+    //verticalCentered: false,
+    //navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+    // responsiveWidth: 900,
+    animateAnchor: false,
+    autoScrolling: false,
+    keyboardScrolling: true,
+    scrollOverflow: true,
+    scrollHorizontally: true,
+    loopHorizontal:false,
+    afterResize: function(width, height){
+  		var fullpageContainer = this;
+  	}
+});
+
+//disabling scrolling down
+fullpage_api.setAllowScrolling(true, 'up, down');
+
+//disabling keyboard scrolling down and right
+fullpage_api.setKeyboardScrolling(true, 'up, down');
+
+// fullpage_api.setScrollingSpeed(0);
+
+//adding the movedown action to the button
+$(document).on('click', '#moveDown', function(){
+  fullpage_api.moveSectionDown();
+});
+
 $('.img-parallax').each(function(){
   var img = $(this);
   var imgParent = $(this).parent();
