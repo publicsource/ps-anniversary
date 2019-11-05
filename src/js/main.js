eaa71a890a70a16f55d76fa0bdde3397d1c61e42
NATASHA VICENS <natasha@NATASHAs-MBP.localdomain>
2019-11-05 11:36:33 -0500

    blank commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 // require("./lib/pym");
 
 ////////// fullPage.js
@@ -16,23 +17,23 @@ var fullpage = require('fullpage.js');
 
 // Initializing it
 var myFullpage = new fullpage('#fullpage', {
-    licenseKey: 'D8303C79-0BD64932-AEC886F3-4E1AAB2D',
-    anchors: ['page1', 'page2', 'page3', 'page4'],
-    sectionsColor: ['#000000', '#ffffff', '#ffffff', '#fffff'],
-    slidesNavigation:true,
-    navigation: true,
-    navigationPosition: 'right',
-    //verticalCentered: false,
-    //navigationTooltips: ['First page', 'Second page', 'Third and last page'],
-    // responsiveWidth: 900,
-    // animateAnchor: false,
-    autoScrolling: false,
-    keyboardScrolling: true,
-    // scrollOverflow: true,
-    loopHorizontal:false,
-    afterResize: function(width, height){
-  		var fullpageContainer = this;
-  	}
+  licenseKey: 'D8303C79-0BD64932-AEC886F3-4E1AAB2D',
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  sectionsColor: ['#000000', '#ffffff', '#ffffff', '#fffff'],
+  slidesNavigation:true,
+  navigation: true,
+  navigationPosition: 'right',
+  //verticalCentered: false,
+  //navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+  // responsiveWidth: 900,
+  // animateAnchor: false,
+  autoScrolling: false,
+  keyboardScrolling: true,
+  // scrollOverflow: true,
+  loopHorizontal:false,
+  afterResize: function(width, height){
+    var fullpageContainer = this;
+  }
 });
 
 //disabling scrolling down
@@ -45,42 +46,42 @@ fullpage_api.setKeyboardScrolling(true, 'up, down');
 
 //adding the movedown action to the button
 $(document).on('click', '#moveDown', function(){
-  fullpage_api.moveSectionDown();
+fullpage_api.moveSectionDown();
 });
 
 $('.img-parallax').each(function(){
-  var img = $(this);
-  var imgParent = $(this).parent();
-  function parallaxImg () {
-    var speed = img.data('speed');
-    var imgY = imgParent.offset().top;
-    var winY = $(this).scrollTop();
-    var winH = $(this).height();
-    var parentH = imgParent.innerHeight();
+var img = $(this);
+var imgParent = $(this).parent();
+function parallaxImg () {
+  var speed = img.data('speed');
+  var imgY = imgParent.offset().top;
+  var winY = $(this).scrollTop();
+  var winH = $(this).height();
+  var parentH = imgParent.innerHeight();
 
 
-    // The next pixel to show on screen
-    var winBottom = winY + winH;
+  // The next pixel to show on screen
+  var winBottom = winY + winH;
 
-    // If block is shown on screen
-    if (winBottom > imgY && winY < imgY + parentH) {
-      // Number of pixels shown after block appear
-      var imgBottom = ((winBottom - imgY) * speed);
-      // Max number of pixels until block disappear
-      var imgTop = winH + parentH;
-      // Porcentage between start showing until disappearing
-      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
-    }
-    img.css({
-      top: imgPercent + '%',
-      transform: 'translate(-50%, -' + imgPercent + '%)'
-    });
+  // If block is shown on screen
+  if (winBottom > imgY && winY < imgY + parentH) {
+    // Number of pixels shown after block appear
+    var imgBottom = ((winBottom - imgY) * speed);
+    // Max number of pixels until block disappear
+    var imgTop = winH + parentH;
+    // Porcentage between start showing until disappearing
+    var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
   }
-  $(document).on({
-    scroll: function () {
-      parallaxImg();
-    }, ready: function () {
-      parallaxImg();
-    }
+  img.css({
+    top: imgPercent + '%',
+    transform: 'translate(-50%, -' + imgPercent + '%)'
   });
+}
+$(document).on({
+  scroll: function () {
+    parallaxImg();
+  }, ready: function () {
+    parallaxImg();
+  }
+});
 });
