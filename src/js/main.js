335892c78f0e4cabc6e2a82fdf4fd5af13b358ef
NATASHA VICENS <natasha@NATASHAs-MBP.localdomain>
2019-11-07 16:29:34 -0500

    fixed partners page

--- src/js/main.js
+++ src/js/main.js
@@ -19,13 +19,13 @@ function fullpageInit(){
       var fullpageContainer = this;
     }
   });
-  
+
   //disabling scrolling down
   fullpage_api.setAllowScrolling(true, 'up, down');
-  
+
   //disabling keyboard scrolling down and right
   fullpage_api.setKeyboardScrolling(true, 'up, down');
-    
+
   //adding the movedown action to the button
   $(document).on('click', '#moveDown', function(){
     fullpage_api.moveSectionDown();
@@ -37,41 +37,3 @@ var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 if(w > 500){
   fullpageInit();
 }
-
-
-$('.img-parallax').each(function(){
-  var img = $(this);
-  var imgParent = $(this).parent();
-  function parallaxImg () {
-    var speed = img.data('speed');
-    var imgY = imgParent.offset().top;
-    var winY = $(this).scrollTop();
-    var winH = $(this).height();
-    var parentH = imgParent.innerHeight();
-  
-  
-    // The next pixel to show on screen
-    var winBottom = winY + winH;
-  
-    // If block is shown on screen
-    if (winBottom > imgY && winY < imgY + parentH) {
-      // Number of pixels shown after block appear
-      var imgBottom = ((winBottom - imgY) * speed);
-      // Max number of pixels until block disappear
-      var imgTop = winH + parentH;
-      // Porcentage between start showing until disappearing
-      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
-    }
-    img.css({
-      top: imgPercent + '%',
-      transform: 'translate(-50%, -' + imgPercent + '%)'
-    });
-  }
-  $(document).on({
-    scroll: function () {
-      parallaxImg();
-    }, ready: function () {
-      parallaxImg();
-    }
-  });
-});
