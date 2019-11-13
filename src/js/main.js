be52a0c090e7312b0f660932722f36e59da9b00e
Alexandra Kanik <alexandra@rationalact.com>
2019-11-13 10:07:31 -0800

    target blank on all story body links

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,14 @@
 
 require("./scrollama");
 
+// not using jquery
+window.onload = function(){
+  var paragraphs = document.getElementById('story-page-body').getElementsByTagName('p');
+  for (var i=0; i<paragraphs.length; i++){
+    var anchors = paragraphs[i].getElementsByTagName('a');
+    for (var x=0; x<anchors.length; x++){
+      anchors[x].setAttribute('target', '_blank');
+    }
+  }
+}
+
